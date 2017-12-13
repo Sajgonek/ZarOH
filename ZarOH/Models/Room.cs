62eaddcf0f4e0c4fca8bcbd6b9a0d14970170ff5
Sajgonek <34008096+Sajgonek@users.noreply.github.com>
2017-12-13 17:47:30 +0100

    Before Creating tables OK

---
+++ ZarOH/Models/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZarOH.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public RoomType RoomType { get; set; }
+        public int RoomTypeId { get; set; }
+    }
+}
