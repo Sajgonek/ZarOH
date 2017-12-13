9e8722c2d7f3cdb87de5a06a9bb5ba02b579ac46
Sajgonek <34008096+Sajgonek@users.noreply.github.com>
2017-12-13 17:10:03 +0100

    Initial Commit

---
+++ ZarOH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZarOH.Startup))]
+namespace ZarOH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
